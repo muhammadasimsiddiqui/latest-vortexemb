166b9407e9f3df437999abf07e8b29d89466bff9
muhammadasimsiddiqui <masimsiddiqui056@gmail.com>
2024-12-29 18:36:31 +0500

    vortexemb

--- resources/views/frontend/partials/faq.blade.php
+++ resources/views/frontend/partials/faq.blade.php
@@ -3,85 +3,84 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="section-title mb-60">
-                        <p class="font-weight-medium caption-font-size mb-6">FAQ's</p>
-                        <h3 class="mb-15">Frequently asked<br/><span>questions!</span></h3>
-                    </div>
+                    <p class="font-weight-medium caption-font-size mb-6">FAQ's</p>
+                    <h3 class="mb-15">Frequently asked<br/><span>questions!</span></h3>
+                </div>
             </div>
             <div class="col-md-6">
                 <div class="accordion" id="faqAccordion">
                     <!-- Question 1 -->
-                    <div class="accordion-item ">
-                      <h2 class="accordion-header" id="faq-heading-1">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-1" aria-expanded="true" aria-controls="faq-collapse-1">
-                          How Much Your Services Cost?
-                        </button>
-                      </h2>
-                      <div id="faq-collapse-1" class="accordion-collapse collapse show" aria-labelledby="faq-heading-1" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                          We usually charge $18 for a cap and left chest logo embroidery digitizing &amp; $5 for one color vector conversion.
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="faq-heading-1">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-1" aria-expanded="true" aria-controls="faq-collapse-1">
+                                How Much Your Services Cost?
+                            </button>
+                        </h2>
+                        <div id="faq-collapse-1" class="accordion-collapse collapse show" aria-labelledby="faq-heading-1">
+                            <div class="accordion-body">
+                                We usually charge $18 for a cap and left chest logo embroidery digitizing &amp; $5 for one color vector conversion.
+                            </div>
                         </div>
-                      </div>
                     </div>
-              
+
                     <!-- Question 2 -->
-                    <div class="accordion-item ">
-                      <h2 class="accordion-header" id="faq-heading-2">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-2" aria-expanded="false" aria-controls="faq-collapse-2">
-                          What Is Your Turnaround Time?
-                        </button>
-                      </h2>
-                      <div id="faq-collapse-2" class="accordion-collapse collapse" aria-labelledby="faq-heading-2" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                          Normally our turnaround is 8-12 hours. However, we prioritize if you need your order urgently.
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="faq-heading-2">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-2" aria-expanded="true" aria-controls="faq-collapse-2">
+                                What Is Your Turnaround Time?
+                            </button>
+                        </h2>
+                        <div id="faq-collapse-2" class="accordion-collapse collapse show" aria-labelledby="faq-heading-2">
+                            <div class="accordion-body">
+                                Normally our turnaround is 8-12 hours. However, we prioritize if you need your order urgently.
+                            </div>
                         </div>
-                      </div>
                     </div>
-              
+
                     <!-- Question 3 -->
-                    <div class="accordion-item ">
-                      <h2 class="accordion-header" id="faq-heading-3">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-3" aria-expanded="false" aria-controls="faq-collapse-3">
-                          How Can I Place My Order?
-                        </button>
-                      </h2>
-                      <div id="faq-collapse-3" class="accordion-collapse collapse" aria-labelledby="faq-heading-3" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                          Register yourself with us or you can send your artwork/image via email <a href="mailto:support@digitemb.com">support@digitemb.com</a>.
-                          <a href="https://www.digitemb.net/" class="btn btn-primary mt-2">Register Now</a>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="faq-heading-3">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-3" aria-expanded="true" aria-controls="faq-collapse-3">
+                                How Can I Place My Order?
+                            </button>
+                        </h2>
+                        <div id="faq-collapse-3" class="accordion-collapse collapse show" aria-labelledby="faq-heading-3">
+                            <div class="accordion-body">
+                                Register yourself with us or you can send your artwork/image via email <a href="mailto:support@digitemb.com">support@digitemb.com</a>.
+                                <a href="https://www.digitemb.net/" class="btn btn-primary mt-2">Register Now</a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-              
+
                     <!-- Question 4 -->
-                    <div class="accordion-item ">
-                      <h2 class="accordion-header" id="faq-heading-4">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-4" aria-expanded="false" aria-controls="faq-collapse-4">
-                          Do You Offer Discount Prices?
-                        </button>
-                      </h2>
-                      <div id="faq-collapse-4" class="accordion-collapse collapse" aria-labelledby="faq-heading-4" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                          Yes, we do offer special discounts to our regular customers, embroidery, and screen printing businesses.
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="faq-heading-4">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-4" aria-expanded="true" aria-controls="faq-collapse-4">
+                                Do You Offer Discount Prices?
+                            </button>
+                        </h2>
+                        <div id="faq-collapse-4" class="accordion-collapse collapse show" aria-labelledby="faq-heading-4">
+                            <div class="accordion-body">
+                                Yes, we do offer special discounts to our regular customers, embroidery, and screen printing businesses.
+                            </div>
                         </div>
-                      </div>
                     </div>
-              
+
                     <!-- Question 5 -->
-                    <div class="accordion-item ">
-                      <h2 class="accordion-header" id="faq-heading-5">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-5" aria-expanded="false" aria-controls="faq-collapse-5">
-                          Do You Offer Free Samples?
-                        </button>
-                      </h2>
-                      <div id="faq-collapse-5" class="accordion-collapse collapse" aria-labelledby="faq-heading-5" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                          Yes, you can find the free embroidery designs in the <a href="https://www.digitemb.com/shop/p-category/free-embroidery-designs/" target="_blank" rel="noopener"><b>freebies</b></a> section.
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="faq-heading-5">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-5" aria-expanded="true" aria-controls="faq-collapse-5">
+                                Do You Offer Free Samples?
+                            </button>
+                        </h2>
+                        <div id="faq-collapse-5" class="accordion-collapse collapse show" aria-labelledby="faq-heading-5">
+                            <div class="accordion-body">
+                                Yes, you can find the free embroidery designs in the <a href="https://www.digitemb.com/shop/p-category/free-embroidery-designs/" target="_blank" rel="noopener"><b>freebies</b></a> section.
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
-  
